611768af1d73ae74564a7762829371ceba94d9d1
Mohammed87FS <mohammedamaar165@gmail.com>
2024-03-07 13:03:25 +0100

    first

---
+++ .netlify/functions-internal/sveltekit-render.mjs
@@ -0,0 +1,37 @@
+import { init } from '../serverless.js';
+
+export const handler = init((() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		client: {"start":"_app/immutable/entry/start.Du2ufE1n.js","app":"_app/immutable/entry/app.C-KkooJy.js","imports":["_app/immutable/entry/start.Du2ufE1n.js","_app/immutable/chunks/entry.Dc4gEaC9.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/entry/app.C-KkooJy.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.fi1e1Nz5.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		nodes: [
+			__memo(() => import('../server/nodes/0.js')),
+			__memo(() => import('../server/nodes/1.js')),
+			__memo(() => import('../server/nodes/2.js'))
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		},
+		server_assets: {}
+	}
+}
+})());
