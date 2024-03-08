30d6dafeb9abb1a79f045d1d865644656bff03a1
Mohammed87FS <84249001+Mohammed87FS@users.noreply.github.com>
2024-03-08 14:22:50 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,16 @@
   
   
   
-    
-  
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>3D Website</title>
+    </head>
+    <body>
       <ThreeScene />
+    </body>
+    </html>
+  
+
   
